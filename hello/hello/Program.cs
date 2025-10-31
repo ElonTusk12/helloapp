2c88d60e6357505bed33228f2c65ea79699a7689
black <black@example.com>
2025-10-31 15:36:34 +0500

    initial commit

---
+++ hello/hello/Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace hello
+{
+    internal class Program
+    {
+        public static void SayHello()
+        {
+            Console.WriteLine("hello");
+        }
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
